b5017da1d32326c30d93c263bcb74f7406b2753e
veyhn <109932151+veyhn@users.noreply.github.com>
2024-05-22 20:40:27 -0400

    Database set up. Switching to python

--- news_database.js
+++ news_database.js
@@ -12,8 +12,6 @@ fs.readFile(file_path, 'utf8', function(err,data){
     const obj = JSON.parse(data)
 });
 
-
-
 // Schema for accessing database
 const {Sequelize, DataTypes, Model} = require('sequelize');
 // initialize database without password / with blank password
@@ -98,10 +96,10 @@ router.get('/database', async function(req,res){
 });
 
 // view a specific column from database
-router.get('/database/column', function(req,res){
+router.get('/database/column', async function(req,res){
     try{
         const column_name = req.query.column;
-        const response = newsData.findAll({
+        const response = await newsData.findAll({
             attributes: `$column_name`,
         });
         res.send(response);
@@ -110,12 +108,12 @@ router.get('/database/column', function(req,res){
     }
 })
 
-// add the one where this one is generalized,then do update and delete, ordering and grouping
+
 // filter/group by category(tech, world news, us news, culture and arts etc.)
-router.get('database/column/category', function(req,res){
+router.get('database/column/category', async function(req,res){
     try{
     const category_name = req.query.category;
-    const response = newsData.findAll({
+    const response = await newsData.findAll({
         group: `${category_name}`
     });
     res.send(response);
@@ -128,7 +126,7 @@ router.get('database/column/category', function(req,res){
 // User can look up headlines
 router.get('database/headline', async function(req,res){
     try{
-        const headline_name = req.query.headline;
+        const headline_name = req.query.keyword;
     response = await  newsData.findAll({
         where: {
             content: {
@@ -136,21 +134,54 @@ router.get('database/headline', async function(req,res){
             }
         }
     });
+    res.json(response); //send response on json 
+    }catch(error){
+        res.status(500).send(`An error occurred while searching for headlines. Error message here: ${error}`);
+    }
+})
+
+// User can look up author
+router.get('database/author', async function(req,res){
+    try{
+        const author_name = req.query.keyword;
+    response = await  newsData.findAll({
+        where: {
+            content: {
+                [Op.like] : `%${author_name}%`
+            }
+        }
+    });
+    res.json(response); //send response on json 
+    }catch(error){
+        res.status(500).send(`An error occurred while searching for headlines. Error message here: ${error}`);
+    }
+})
+
+// Filter news according to date (format: YYY-M-D)
+router.get('/database/date', async function(req,res){
+    const {startDate, endDate} = req.query; //receive dates from user
+
+    // Validate user date inputs
+    if (!startDate || !endDate){
+        return res.status(500).json({message: `Please provide both a start date and end date. Error: ${error}`});
+
+    }
+    try{
+        const response = await newsData.findAll({
+            where:{
+                createdAt:{
+                    [Sequelize.Op.between]: [new Date(startDate), new Date(endDate)]
+                }
+            }
+        });
+        res.json(response)
     }catch(error){
         res.status(500).send(error);
     }
-
-})
+}) 
 
 module.exports = router;
 
-// Fix these issues later
-// async function sync(){
-//     await sequelize.sync({force:true});
-// console.log('All models were synchronized successfully');
-
-// } 
 
 
-// modules.default.export = {newsData};
 
